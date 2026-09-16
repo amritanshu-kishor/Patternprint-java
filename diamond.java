721ab3f523872cceec555938e038d8c1d00e410f
Amritanshu Kishor <amritkis2006@gmail.com>
2026-09-16 22:46:17 +0530

    feat: diamond

---
+++ diamond.java
@@ -0,0 +1,32 @@
+package practicepattern;
+import java.util.*;
+public class diamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int nst =1, nsp =n-1;
+        for(int i =1;i<=n;i++) {
+            for (int j=1;j<=nsp;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=nst;j++){
+                System.out.print("* ");
+            }
+            nsp--;
+            nst+=2;
+            System.out.println();
+        }
+        nst =2*n-3; nsp = 1 ;
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=nsp;j++){
+                System.out.print("  ");
+            }
+            for(int j =1;j<=nst;j++){
+                System.out.print("* ");
+            }
+            nsp++;
+            nst -=2;
+            System.out.println();
+        }
+    }
+}
