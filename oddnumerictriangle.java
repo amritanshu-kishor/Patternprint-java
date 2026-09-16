4bdf396372dfe87b242283d3411a99ec2d040f97
Amritanshu Kishor <amritkis2006@gmail.com>
2026-09-16 21:04:22 +0530

    feat: odd numeric triangle

---
+++ oddnumerictriangle.java
@@ -0,0 +1,14 @@
+package practicepattern;
+import java.util.*;
+public class oddnumerictriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=i;j++) {
+                System.out.print(2*j-1+" ");
+            }
+            System.out.println("  ");
+        }
+    }
+}
